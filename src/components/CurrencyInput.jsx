effd35138981d09d838c8f4e3d3256e28e6001e3
Ricardo Nava <ricardo.a.nava@gmail.com>
2018-11-08 19:31:13 -0800

    Added CurrencyInput no functionality yet

--- src/components/CurrencyInput.jsx
+++ src/components/CurrencyInput.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CurrencyInput = () => <p>CurrencyInput.jsx</p>;
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
 
-export default CurrencyInput;
+const styles = () => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  textField: {
+    width: 960,
+  },
+});
+
+const CurrencyInput = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <TextField
+        id="standard-number"
+        label="MXN"
+        value={100.5}
+        // onChange={this.handleChange('age')}
+        type="number"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+      />
+    </div>
+  );
+};
+
+CurrencyInput.propTypes = {
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles(styles)(CurrencyInput);
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 
+import CurrencyInput from '../components/CurrencyInput';
 import CurrencyCard from '../components/CurrencyCard';
 
 const styles = theme => ({
   main: {
+    width: 'auto',
     margin: theme.spacing.unit * 3,
   },
 });
@@ -17,6 +19,7 @@ const Main = (props) => {
 
   return (
     <main className={classes.main}>
+      <CurrencyInput />
       <Grid container spacing={16} justify="center">
         {[1, 2, 3, 4, 5, 6].map(() => (
           <Grid item xs={12} sm={6} md={4} lg={4}>
